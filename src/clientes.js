4b649dc97a803c19a05dc963f613ca358bc191f1
nicolellopezh <nicolelh9.nl@gmail.com>
2026-09-16 21:54:28 -0500

    feat: agregar informacion inicial de cliente

--- src/clientes.js
+++ src/clientes.js
@@ -1,1 +1,8 @@
-Módulo Clientes
+//Módulo Clientes
+// Información inicial del módulo de clientes
+
+const cliente = {
+    nombre: "Nicole Lopez",
+    correo: "nicole@gmail.com",
+    estado: "Activo"
+};
